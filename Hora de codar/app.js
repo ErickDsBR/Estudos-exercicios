295526b128177130dc96509b874a3efb0aa2314e
ErickDsBR <erick.brasil2102@gmail.com>
2025-08-20 19:01:36 -0300

    mostrar texto na tela

--- Hora de codar/app.js
+++ Hora de codar/app.js
@@ -1,2 +1,13 @@
-const campotext = document.getElementById("campotext").value;
-console.log(campotext)
+const h2 = document.querySelector('h2')
+const campotext = document.getElementById("campotext");
+const botaoenviar = document.getElementById("button");
+
+botaoenviar.addEventListener('click', function() {
+   const newtext = campotext.value;
+    
+   if (newtext.trim()!==''){
+    h2.textContent = newtext;
+
+   };
+    
+});
