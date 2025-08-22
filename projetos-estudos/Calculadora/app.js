45c41bf6ed1f30728f3ec86970e7814abd764d99
ErickDsBR <erick.brasil2102@gmail.com>
2025-08-21 21:16:56 -0300

    2h por um espaço no return

--- projetos-estudos/Calculadora/app.js
+++ projetos-estudos/Calculadora/app.js
@@ -1,12 +1,22 @@
 function insertToDisplay(data){    
-    document.querySelector('#dp').value+= data
+    document.querySelector('#dp').value += data
     
 };
+
 function limpar(){
-     document.querySelector("#dp").value = '';
+    document.querySelector("#dp").value = '';
 };
- function back(){
+function back(){
     const dp = document.querySelector('#dp')
     dp.value = dp.value.slice(0, -1)
- }
+}
+function novovalor(){
+    const expressao_dp = document.querySelector('#dp').value  
+    
+    if(expressao_dp) {
+        const resultado = new Function('return ' + expressao_dp)();   
+        document.querySelector('#dp').value = resultado
+    }      
+}
+
  
