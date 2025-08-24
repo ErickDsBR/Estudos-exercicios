d48ee4d57584de4326d89eb71e51ca395bf88d63
ErickDsBR <erick.brasil2102@gmail.com>
2025-08-24 18:32:23 -0300

    to-do-list

--- projetos-estudos/To-do-List/app.js
+++ projetos-estudos/To-do-List/app.js
@@ -1,10 +1,21 @@
 function salvartarefa(){    
+    //este captura o valor do input no html trazendo o valor para ser manipulado no html
     const inputTarefa = document.getElementById('tarefa').value
+    // esta entrando valor vazio na lista ainda par resolver...
+    if(inputTarefa){
+        inputTarefa === ''
+        document.getElementById('erro').innerText = 'erro'
+    }
+
+    //selecionar aonde vai ser adicioado o elemento para lista
     const listaTarefa = document.getElementById('lista')
+
+    // createElement serve para criar um elemento html 
     const novaTarefa = document.createElement('li')
 
     novaTarefa.textContent = inputTarefa
 
+    // appendChild adicionar o elemento ao html aonde foi selecionado no nova tarefa
     listaTarefa.appendChild(novaTarefa)
 
 }
