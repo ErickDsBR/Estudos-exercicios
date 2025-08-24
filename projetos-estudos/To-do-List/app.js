ef31ffd59a06f17d2936fbae62fe050a1b1e66cd
ErickDsBR <erick.brasil2102@gmail.com>
2025-08-24 16:49:38 -0300

    to-do-list

---
+++ projetos-estudos/To-do-List/app.js
@@ -0,0 +1,8 @@
+function salvartarefa(){
+    const nomeTarefa = document.getElementById('tarefa').value 
+    localStorage.setItem('Tarefas', nomeTarefa);        
+}
+function trfcomplete(){
+
+
+}
