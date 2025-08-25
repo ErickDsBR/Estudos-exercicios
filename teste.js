97972ed7fbf62664e23b853216b9275bc152d4da
ErickDsBR <erick.brasil2102@gmail.com>
2025-08-25 17:33:05 -0300

    manipulação de array

---
+++ teste.js
@@ -0,0 +1,9 @@
+const nomelista = document.querySelector().value;
+    let a = 'pamonha'; // variavel de teste
+    const lista = [
+        
+    ];
+    lista.push({nome: nomelista });
+    const nomes = lista.map(list => list.nome);
+    localStorage.setItem(nomes);
+    
